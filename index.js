91d435376bf281d939638c99b866db25895ad781
Yassine <mekoyassine04@gmail.com>
2026-02-02 07:50:11 +0700

    final railway fix

--- index.js
+++ index.js
@@ -2,23 +2,34 @@ const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-    puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    }
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu'
+    ]
+  }
 });
 
 client.on('qr', qr => {
-    qrcode.generate(qr, { small: true });
+  qrcode.generate(qr, { small: true });
+  console.log('📱 Scan QR Code');
 });
 
 client.on('ready', () => {
-    console.log('🤖 البوت خدام 24/24!');
+  console.log('✅ البوت خدام 24/24');
 });
 
 client.on('message', message => {
-    if (message.body === 'السلام') {
-        message.reply('وعليكم السلام 👋 أنا بوت واتساب عربي');
-    }
+  if (message.body === 'السلام') {
+    message.reply('وعليكم السلام 👋 أنا بوت واتساب');
+  }
 });
 
 client.initialize();
