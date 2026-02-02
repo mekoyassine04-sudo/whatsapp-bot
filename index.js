e792ffb85af40e22e23d9b79d8b89378b4e9a563
mekoyassine04-sudo <mekoyassine04@gmail.com>
2026-02-03 02:44:11 +0700

    Update Puppeteer configuration in index.js

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ const qrcode = require('qrcode-terminal');
 const client = new Client({
   puppeteer: {
     headless: true,
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
     args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process',
-      '--disable-gpu'
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+      "--single-process"
     ]
   }
 });
